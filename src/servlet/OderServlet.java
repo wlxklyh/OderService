2fa6cb37d1a8f09b95b6d501fa7892b647f6c525
wlxklyh <linyanhou@qq.com>
2014-07-09 02:06:14 +0800

    post分发
    post只有一个就是提交订单

--- src/servlet/OderServlet.java
+++ src/servlet/OderServlet.java
@@ -1,6 +1,9 @@
 package servlet;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.net.URLDecoder;
 import java.util.ResourceBundle;
 
 import javax.servlet.ServletException;
@@ -14,11 +17,20 @@ import database.JDBC;
 
 public class OderServlet extends HttpServlet {
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse resp)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		super.doPost(request, resp);
+		response.setContentType("text/html");
+	    response.setCharacterEncoding("utf-8");
+		super.doPost(request, response);
+
 		
+		String tempStr=request.getQueryString();
+		System.out.println("doPost"+tempStr);
+
+		String jsonString=request.getParameter("jsonString");
+		String tag=URLDecoder.decode(jsonString,"Utf-8");
+		System.out.println(tag);
 		
 		
 	}
@@ -31,6 +43,11 @@ public class OderServlet extends HttpServlet {
 	    response.setCharacterEncoding("utf-8");
 		PrintWriter out = response.getWriter();
 		String method = request.getParameter("a");
+		
+		String tempStr=request.getQueryString();
+		System.out.println("doGet1"+tempStr);
+		
+		
 		if(method.equals("login")){
 			String phone=request.getParameter("phone");
 		   	String pwd=request.getParameter("password");
@@ -49,8 +66,20 @@ public class OderServlet extends HttpServlet {
 			System.out.println(phone);
 			out.print(JDBC.getFoodList(phone).toString());
 			System.out.println(JDBC.getFoodList(phone).toString());
-		}else if(method.equals("login1")){
+		}else if(method.equals("order")){
 			
+			tempStr=request.getQueryString();
+			System.out.println("doGet2"+tempStr);
+
+			String jsonString=request.getParameter("jsonString");
+			String tag=URLDecoder.decode(jsonString,"Utf-8");
+			System.out.println(tag);
+//			
+//			InputStream is = request.getInputStream(); 
+//			DataInputStream input = new DataInputStream(is); 
+//			String str =input.readUTF(); 
+//			
+//			System.out.println(str);
 		}else if(method.equals("login1")){
 			
 		}else if(method.equals("login1")){
