53e2841ff1adaf49e01466f9f450ba9f0939e65a
jiqiujia <jiqiujia2014@gmail.com>
2014-07-09 22:09:54 +0800

    OrderList.java

---
+++ src/data/OrderList.java
@@ -0,0 +1,34 @@
+package data;
+
+public class OrderList {
+	String orderId;
+	String foodName;
+	String status;
+	int num;
+	public String getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+	public String getFoodName() {
+		return foodName;
+	}
+	public void setFoodName(String foodName) {
+		this.foodName = foodName;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	
+}
